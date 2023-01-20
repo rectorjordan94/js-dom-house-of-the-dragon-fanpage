bbf573d7cf332776b9438839424f3a115e18915b
rectorjordan94 <rectorjordan94@gmail.com>
2023-01-20 08:59:41 -0600

    adds search functionality

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const rhaenyraCard = document.getElementById('rhaenyraCard')
+const alicentCard = document.getElementById('alicentCard')
+const viserysCard = document.getElementById('viserysCard')
+const daemonCard = document.getElementById('daemonCard')
+const corlysCard = document.getElementById('corlysCard')
+const rhaenysCard = document.getElementById('rhaenysCard')
 const rhaenyraBioButton = document.getElementById('rhaenyra-bio-button')
 const rhaenyraAOD = document.getElementById('rhaenyraAOD')
 const rhaenyraStatusButton = document.getElementById('rhaenyra-status-button')
+const searchInput = document.getElementById('search-input')
+const bioButtons = document.querySelectorAll('.bio')
 
 const displayRhaenyraBio = () => {
     const bioContainer = document.createElement('div')
@@ -19,4 +26,45 @@ const displayRhaenyraStatus = () => {
 
 rhaenyraBioButton.addEventListener('click', displayRhaenyraBio , {once: true})
 
-rhaenyraStatusButton.addEventListener('click', displayRhaenyraStatus, {once: true})
+rhaenyraStatusButton.addEventListener('click', displayRhaenyraStatus, { once: true })
+
+searchInput.addEventListener('keydown', (e) => {
+    const cards = document.querySelectorAll('.card')
+    if (e.code === 'Enter') {
+        if (searchInput.value.toLowerCase() == 'rhaenyra') {
+            cards.forEach(card => card.classList.add('hidden'))
+            rhaenyraCard.classList.remove('hidden')
+            rhaenyraCard.style.gridArea = 'alicent'
+        } else if (searchInput.value.toLowerCase() == 'alicent') {
+            cards.forEach(card => card.classList.add('hidden'))
+            alicentCard.classList.remove('hidden')
+        } else if (searchInput.value.toLowerCase() == 'viserys') {
+            cards.forEach(card => card.classList.add('hidden'))
+            viserysCard.classList.remove('hidden')
+            viserysCard.style.gridArea = 'alicent'
+        } else if (searchInput.value.toLowerCase() == 'daemon') {
+            cards.forEach(card => card.classList.add('hidden'))
+            daemonCard.classList.remove('hidden')
+            daemonCard.style.gridArea = 'alicent'
+        } else if (searchInput.value.toLowerCase() == 'corlys') {
+            cards.forEach(card => card.classList.add('hidden'))
+            corlysCard.classList.remove('hidden')
+            corlysCard.style.gridArea = 'alicent'
+        } else if (searchInput.value.toLowerCase() == 'rhaenys') {
+            cards.forEach(card => card.classList.add('hidden'))
+            rhaenysCard.classList.remove('hidden')
+            rhaenysCard.style.gridArea = 'alicent'
+        }
+        // const returnButton = document.createElement('button')
+        // corlysCard.appendChild(returnButton)
+    }
+    // } else if (searchInput.value = '') {
+    //     cards.forEach(card => card.classList.remove('hidden'))
+    //     rhaenyraCard.style.gridArea = 'rhaenyra'
+    //     alicentCard.style.gridArea = 'alicent'
+    //     viserysCard.style.gridArea = 'viserys'
+    //     daemonCard.style.gridArea = 'daemon'
+    //     corlysCard.style.gridArea = 'corlys'
+    //     rhaenysCard.style.gridArea = 'rhaenys'
+    // }
+})
